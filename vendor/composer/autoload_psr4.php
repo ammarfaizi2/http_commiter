c1de36a0bae7a01e52daaff3d238c13b85b7ddda
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-28 13:07:18 +0700

    composer dumpautoload

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'System\\' => array($baseDir . '/System'),
     'Handler\\' => array($baseDir . '/Handler'),
 );
