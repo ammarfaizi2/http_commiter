926f134971b92e3e33a1ac8ca3764beb631b93a6
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-28 19:10:11 +0700

    Fix

--- Handler/ActionHandler.php
+++ Handler/ActionHandler.php
@@ -26,7 +26,7 @@ class ActionHandler
 	public function run()
 	{
 		#$this->login_action();
-		for($i=0;$i<=3;$i++){
+		for($i=0;$i<=10;$i++){
 		$this->github->edit_file("https://github.com/ammarfaizi2/rgp/edit/master/index.py","print \"".rand(0,99999)."\"");
 sleep(2);
 }
