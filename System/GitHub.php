e4fb8f65bc9d8878c2dade6e0547c2639a3a9a05
Ammar Faizi <ammarfaizi2@gmail.com>
2017-05-28 13:30:38 +0700

    Update GitHub login

--- System/GitHub.php
+++ System/GitHub.php
@@ -10,15 +10,17 @@ class GitHub
 	{
 		$this->user = $user;
 		$this->pass = $pass;
+		$this->hash = md5($user.$pass);
 	}
 
 	public function login()
 	{
-		/*$ch = new CMCurl("https://github.com/login");
+		$ch = new CMCurl("https://github.com/login");
 		$ch->set_useragent("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:46.0) Gecko/20100101 Firefox/46.0");
-		$src = $ch->execute();*/
-		$a = file_get_contents('a.tmp');
-		$a = explode("<form", $a);
+		$ch->set_cookie(data . "/" . $this->hash . ".txt");
+		$src = $ch->execute();
+		#$a = file_get_contents('a.tmp');
+		$a = explode("<form", $src);
 		$a = explode("</form", $a[1]);
 		$a = explode("<input", $a[0]);
 		$_p = "commit=Sign+in&";
